f362f8011d95717109962f78294e1d9a77a41d08
tenorok <mail@tenorok.ru>
2014-12-18 18:21:16 +0300

    Model declaration with properties `modName` and `modVal` not affect declarations without modifier.

--- test/modifiers.js
+++ test/modifiers.js
@@ -2,46 +2,77 @@ describe('The declaration blocks with modifiers.', function() {
 
     describe('The declaration block with only `modName` property.', function() {
         var block,
-            blockDecl = { block: 'component', modName: 'disabled' };
+            blockMod,
+            blockModDecl = { block: 'component', modName: 'disabled' };
 
-        BEM.decl(blockDecl, {});
-        BEM.model(blockDecl, {
+        BEM.decl('component', {});
+        BEM.decl(blockModDecl, {});
+        BEM.model(blockModDecl, {
             size: { value: 'large' }
         });
 
         beforeEach(function() {
-            block = BEM.create({ block: 'component', mods: { disabled: true }});
+            block = BEM.create('component');
+            blockMod = BEM.create({ block: 'component', mods: { disabled: true }});
         });
 
         afterEach(function() {
             block = null;
+            blockMod = null;
         });
 
-        it('Property `name` should be preset', function() {
-            assert.equal(block.model('size'), 'large');
+        it('Block without modifier should not have property `size`', function() {
+            assert.isUndefined(block.model('size'));
+        });
+
+        it('Property `size` should be preset', function() {
+            assert.equal(blockMod.model('size'), 'large');
         });
 
     });
 
     describe('The declaration block with both `modName` and `modVal` properties.', function() {
         var block,
+            blockMod,
+            blockModVal,
             blockDecl = { block: 'component', modName: 'type', modVal: 'text' };
 
+        BEM.decl('component', {});
+        BEM.decl({ block: 'component', modName: 'type' }, {});
         BEM.decl(blockDecl, {});
         BEM.model(blockDecl, {
-            val: { value: 'Yo!' }
+            val: { value: 'Yo' }
+        });
+        BEM.model(blockDecl, {
+            val: { value: 'Yo!' },
+            count: { value: 50 }
         });
 
         beforeEach(function() {
-            block = BEM.create({ block: 'component', mods: { type: 'text' }});
+            block = BEM.create('component');
+            blockMod = BEM.create({ block: 'component', mods: { type: true }});
+            blockModVal = BEM.create({ block: 'component', mods: { type: 'text' }});
         });
 
         afterEach(function() {
             block = null;
+            blockMod = null;
+            blockModVal = null;
         });
 
-        it('Property `name` should be preset', function() {
-            assert.equal(block.model('val'), 'Yo!');
+        it('Block without modifier should not have property `val`', function() {
+            assert.isUndefined(block.model('val'));
+            assert.isUndefined(block.model('count'));
+        });
+
+        it('Block without modifier value should not have property `val`', function() {
+            assert.isUndefined(blockMod.model('val'));
+            assert.isUndefined(blockMod.model('count'));
+        });
+
+        it('Property `val` should be preset with overridden value', function() {
+            assert.equal(blockModVal.model('val'), 'Yo!');
+            assert.equal(blockModVal.model('count'), 50);
         });
 
     });
